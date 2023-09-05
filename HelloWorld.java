4f1fad2c60cb1b71c6e01800a4b240f282e5eca7
kruger.joshua <109550490+krugerjoshua@users.noreply.github.com>
2023-09-05 21:35:18 +0200

    Create HelloWorld.java
    First project to view the basic print functions.

---
+++ HelloWorld.java
@@ -0,0 +1,43 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Tim");
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am Scared of aliens");
+        }
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 95;
+        if ((topScore > secondTopScore) && (topScore < 100)) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is an error");
+        }
+
+        boolean isCar = false;
+        if (isCar == true) {
+            System.out.println("This is not supposed to happen.");
+        }
+
+        String makeOfCar = "Volkswagen";
+        boolean isDomestic = makeOfCar == "Volkswagen" ? false : true;
+
+        if (isDomestic) {
+            System.out.println("This car is domestic to our country");
+        }
+    }
+}
